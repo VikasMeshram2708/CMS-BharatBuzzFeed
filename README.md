f86b9f34ed888038fedd9d5015274e39c4311c36
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-02-20 20:01:46 +0530

    :tada: Created api for india page.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CMS (Content Management System) Tool for Bharat Buzz Feed [https://www.bharatbuzzfeed.com]
 
 - This is a Content Management System for news website called BharatBuzzFeed.
+
+### API Completed for Pages
+
+- [x] Home.
+- [] India.
--- routes/all.route.ts
+++ routes/all.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import HomePageController from "../controllers/home.controller";
 import FilterNewsController from "../controllers/filter.controller";
+import IndiaPageController from "../controllers/india.controller";
 
 const router = express.Router();
 
 // HOME page api
 
-router.get("/home-page", HomePageController);
+router.post("/home-page", HomePageController);
 router.get("/filter-news/:id", FilterNewsController);
+router.post("/india-page", IndiaPageController);
 
 export default router;
